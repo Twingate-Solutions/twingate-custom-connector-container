5c7c8908d79825ffbfff1855e23fd7fdd2e03324
bcameron-twingate <bcameron@twingate.com>
2026-01-09 11:32:00 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+CONFIG="/etc/twingate/connector.conf"
+CONFIG_DIR=$(dirname "$CONFIG")
+
 log_with_timestamp() {
   echo "[$(date '+%Y-%m-%d %H:%M:%S')] $@"
 }
@@ -12,7 +15,38 @@ log_with_timestamp "[entrypoint] Installing Twingate connector..."
 : "${TWINGATE_REFRESH_TOKEN:?required}"
 : "${TWINGATE_NETWORK:?required}"
 
-curl -fsSL https://binaries.twingate.com/connector/setup.sh | TWINGATE_ACCESS_TOKEN="$TWINGATE_ACCESS_TOKEN" TWINGATE_REFRESH_TOKEN="$TWINGATE_REFRESH_TOKEN" TWINGATE_NETWORK="$TWINGATE_NETWORK" TWINGATE_LABEL_DEPLOYED_BY="custom_docker_connector_v1" bash
+# Install the Twingate Connector
+apt update -yq
+apt_require_curl
+apt_require_gpg
+TWINGATE_GPG_PUBLIC_KEY=/usr/share/keyrings/twingate-connector-keyring.gpg
+if ! curl -fsSL "https://packages.twingate.com/apt/gpg.key" | gpg --dearmor -o "$TWINGATE_GPG_PUBLIC_KEY"; then
+    echo "Failed to download or process GPG key"
+    exit 1
+fi
+echo "deb [signed-by=${TWINGATE_GPG_PUBLIC_KEY}] https://packages.twingate.com/apt/ /" | tee /etc/apt/sources.list.d/twingate.list
+apt update -yq
+apt install -yq twingate-connector
+
+if [ -n "${TWINGATE_NETWORK}" ]; then
+  echo "TWINGATE_NETWORK=${TWINGATE_NETWORK}" >> "${CONFIG}"
+elif [ -n "${TWINGATE_URL}" ]; then
+  echo "TWINGATE_URL=${TWINGATE_URL}" >> "${CONFIG}"
+fi
+
+if [ -n "${TWINGATE_ACCESS_TOKEN}" ] && [ -n "${TWINGATE_REFRESH_TOKEN}" ]; then
+    { \
+        echo "TWINGATE_ACCESS_TOKEN=${TWINGATE_ACCESS_TOKEN}"; \
+        echo "TWINGATE_REFRESH_TOKEN=${TWINGATE_REFRESH_TOKEN}"; \
+    } >> "${CONFIG}"
+    if [ -n "${TWINGATE_LOG_ANALYTICS}" ]; then
+        echo "TWINGATE_LOG_ANALYTICS=${TWINGATE_LOG_ANALYTICS}" >> "${CONFIG}"
+    fi
+    chmod 0600 "$CONFIG"
+    systemctl enable --now twingate-connector
+fi
+
+echo "TWINGATE_LABEL_DEPLOYED_BY=custom_docker_connector_v1" | sudo tee -a /etc/twingate/connector.conf
 
 if [ "$TWINGATE_LOG_ANALYTICS" ]; then
   echo "Enabling detailed traffic logging"
@@ -27,7 +61,7 @@ else
 fi
 
 log_with_timestamp "[entrypoint] Starting Twingate connector container..."
-systemctl restart twingate-connector.service
+exec twingate-connector
 
 log_with_timestamp "[entrypoint] Twingate started. Keeping container running."
 log_with_timestamp "[entrypoint] Forwarding Twingate log to stdout..."
